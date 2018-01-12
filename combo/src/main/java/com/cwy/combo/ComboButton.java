2ccf32fe8095532c8a37b951b4ed2279ea133979
cwyrzz <309107361@qq.com>
2018-01-12 22:31:37 +0800

    update button

--- combo/src/main/java/com/cwy/combo/ComboButton.java
+++ combo/src/main/java/com/cwy/combo/ComboButton.java
@@ -1,6 +1,9 @@
 package com.cwy.combo;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -8,14 +11,58 @@ import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
 
 /**
  * Created by Chenwy on 2018/1/12 14:34
  */
 
-public class ComboButton extends View {
+public class ComboButton extends View implements View.OnClickListener {
+    /**
+     * 默认宽高尺寸
+     */
     private final int DEFAULT_SIZE = 100;
+
+    /**
+     * 默认动画时间
+     */
+    private final int DEFAULT_ANIM_DURATION = 300;
+
+    /**
+     * 画笔
+     */
     private Paint paint;
+
+    /**
+     * 动画时间
+     */
+    private int animDuration;
+
+    /**
+     * 最底下圆的颜色
+     */
+    private int underCircleColor;
+
+    /**
+     * 中间圆的颜色
+     */
+    private int centerCircleColor;
+
+    /**
+     * 最顶部圆的颜色
+     */
+    private int topCircleColor;
+
+    /**
+     * 圆与圆之间的间距
+     */
+    private float circleSpacing;
+
+    /**
+     * 缩放倍数
+     */
+    private float zoomF;
 
     public ComboButton(Context context) {
         this(context, null);
@@ -28,6 +75,16 @@ public class ComboButton extends View {
     public ComboButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         paint = new Paint();
+
+        TypedArray t = context.obtainStyledAttributes(attrs, R.styleable.ComboButton);
+        animDuration = t.getInteger(R.styleable.ComboButton_animDuration, DEFAULT_ANIM_DURATION);
+        underCircleColor = t.getColor(R.styleable.ComboButton_underCircleColor, Color.parseColor("#E291C5"));
+        centerCircleColor = t.getColor(R.styleable.ComboButton_centerCircleColor, Color.parseColor("#E863CF"));
+        topCircleColor = t.getColor(R.styleable.ComboButton_topCircleColor, Color.parseColor("#EC48DA"));
+        circleSpacing = t.getDimension(R.styleable.ComboButton_circleSpacing, dp2px(8));
+        zoomF = t.getFloat(R.styleable.ComboButton_zoomF, 1.5f);
+        t.recycle();
+        setOnClickListener(this);
     }
 
     @Override
@@ -57,19 +114,19 @@ public class ComboButton extends View {
         int centerY = firstR;
 
         //绘制第一个圆
-        paint.setColor(Color.parseColor("#E291C5"));
+        paint.setColor(underCircleColor);
         canvas.drawCircle(firstR, centerX, centerY, paint);
 
         //中间圆半径
-        int centerR = firstR - dp2px(8f);
+        float centerR = firstR - circleSpacing;
         //绘制中间圆
-        paint.setColor(Color.parseColor("#E863CF"));
+        paint.setColor(centerCircleColor);
         canvas.drawCircle(centerX, centerY, centerR, paint);
 
         //最外层圆
-        int outR = centerR - dp2px(8f);
+        float outR = centerR - circleSpacing;
         //绘制最外层圆
-        paint.setColor(Color.parseColor("#EC48DA"));
+        paint.setColor(topCircleColor);
         canvas.drawCircle(centerX, centerY, outR, paint);
 
         String text = "连击";
@@ -110,5 +167,16 @@ public class ComboButton extends View {
     public int dp2px(float dpValue) {
         final float scale = getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
+    }
+
+    @Override
+    public void onClick(View view) {
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(this, "scaleX", 1f, zoomF, 1f);
+        ObjectAnimator scaleY = ObjectAnimator.ofFloat(this, "scaleY", 1f, zoomF, 1f);
+        AnimatorSet set = new AnimatorSet();
+        set.play(scaleX).with(scaleY);
+        set.setDuration(animDuration);
+        set.setInterpolator(new OvershootInterpolator());
+        set.start();
     }
 }
